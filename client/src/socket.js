ef8f9e85a99213c94da97b854c2627a281ae9655
vikasbhatt12 <vikasbhatt12082003@gmail.com>
2024-07-05 15:47:35 +0530

    added new server connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"; // import connection function
 
-const socket = io('localhost:8080'); // initialize websocket connection
+const socket = io('https://elitechess.onrender.com/'); // initialize websocket connection
 
 export default socket;
